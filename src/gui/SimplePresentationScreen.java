f80d695c6862a831be254d9ea847fbeeea644312
juancruz harguindeguy <juancruzhar123@gmail.com>
2021-09-02 23:50:18 -0300

    segundo commit

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -2,13 +2,22 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.*;
+
 
 import entities.Student;
-import javax.swing.JTabbedPane;
+
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.border.LineBorder;
+import java.awt.SystemColor;
+import java.awt.Color;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -17,29 +26,123 @@ public class SimplePresentationScreen extends JFrame {
 	private JPanel tabInformation;
 	private JTabbedPane tabbedPane;
 	private Student studentData;
-
+	private JTextField textField;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JTextField textField_1;
+	private JLabel lblNewLabel_2;
+	private JTextField textField_2;
+	private JLabel lblNewLabel_3;
+	private JTextField textField_3;
+	private JLabel lblNewLabel_4;
+	private JTextField txtAsd;
+	
+	
 	public SimplePresentationScreen(Student studentData) {
 		this.studentData = studentData;
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentaciÃ³n");
+		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentación");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(new Dimension(615, 250));
+		setSize(new Dimension(610, 240));
 		setResizable(false);
 		setContentPane(contentPane);
 		
 		init();
 	}
-	
+
 	private void init() {
+		contentPane.setLayout(null);
+		
 		// Tabbed Pane to student personal data
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 11, 430, 165);
 		tabInformation = new JPanel();
-		tabInformation.setPreferredSize(new Dimension(425, 275));
-		tabbedPane.addTab("InformaciÃ³n del alumno", null, tabInformation, "Muestra la informaciÃ³n declarada por el alumno");
-		contentPane.add(tabbedPane, BorderLayout.WEST);
+		tabInformation.setBorder(new LineBorder(SystemColor.textHighlight));
+		FlowLayout flowLayout = (FlowLayout) tabInformation.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		tabInformation.setPreferredSize(new Dimension(425, 200));
+		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
+		tabbedPane.setForegroundAt(0, Color.BLACK);
+		tabbedPane.setBackgroundAt(0, Color.RED);
+
+		
+		lblNewLabel = new JLabel("LU                    ");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		tabInformation.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setHorizontalAlignment(SwingConstants.LEFT);
+		tabInformation.add(textField);
+		textField.setColumns(35);
+		
+		lblNewLabel_1 = new JLabel("Apellido           ");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		tabInformation.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		tabInformation.add(textField_1);
+		textField_1.setColumns(35);
+		
+		lblNewLabel_2 = new JLabel("Nombre           ");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 11));
+		tabInformation.add(lblNewLabel_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setEditable(false);
+		tabInformation.add(textField_2);
+		textField_2.setColumns(35);
+		
+		lblNewLabel_3 = new JLabel("E-mail              ");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 11));
+		tabInformation.add(lblNewLabel_3);
+		
+		textField_3 = new JTextField();
+		textField_3.setEditable(false);
+		tabInformation.add(textField_3);
+		textField_3.setColumns(35);
+		
+		lblNewLabel_4 = new JLabel("GitHub URL     ");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 11));
+		tabInformation.add(lblNewLabel_4);
+		
+		txtAsd = new JTextField();
+		txtAsd.setEditable(false);
+		tabInformation.add(txtAsd);
+		txtAsd.setColumns(35);
+		contentPane.add(tabbedPane);
+		
+		
+		
+		DateTimeFormatter fTime = DateTimeFormatter.ofPattern("h':'mm':'s");
+		DateTimeFormatter fDate = DateTimeFormatter.ofPattern("d'/'MM'/'YY");
+		JLabel lblNewLabel_6 = new JLabel("Esta ventana fue generada el "+String.valueOf(LocalDate.now().format(fDate))+" a las: "+String.valueOf(LocalTime.now().format(fTime)));
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_6.setBounds(10, 187, 430, 14);
+		contentPane.add(lblNewLabel_6);
+		
+		
+		
+		
+		JLabel lblNewLabel_5 = new JLabel("");
+		lblNewLabel_5.setBounds(448, 26, 128, 128);
+		lblNewLabel_5.setIcon(new ImageIcon(new javax.swing.ImageIcon(getClass().getResource(studentData.getPathPhoto())).getImage().getScaledInstance(155, 140, Image.SCALE_SMOOTH)));
+		contentPane.add(lblNewLabel_5);
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,13 +1,23 @@
 package launcher;
 
+import java.awt.Toolkit;
+
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+            	Student s1 = new Student(43195779,"Harguindeguy","Juan Cruz","juancruzhar123@gmail.com","https://github.com/JuancruzHarguindeguy","/images/trol.png");
             	
+            	SimplePresentationScreen s = new SimplePresentationScreen(s1);
+            	s.setVisible(true);
+            	s.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/images/tdp.png")));
             }
+            
         });
     }
 }
